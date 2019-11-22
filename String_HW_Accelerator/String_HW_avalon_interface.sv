a2615e3cdd24ffdcc1bf868d5963bbe871aa6b59
matthew46 <matthew.bowen464@yahoo.com>
2019-11-22 10:37:15 -0600

    Updates and Test Bench

--- String_HW_Accelerator/String_HW_avalon_interface.sv
+++ String_HW_Accelerator/String_HW_avalon_interface.sv
@@ -1,9 +1,9 @@
 /* 
  * ###############################################################################
  * CPE423
- * Greatest Common Divisor AVALON HW Interface
- * David Tougaw 
- * 10/15/2019
+ * String Hardware Avalon INTERFACE--------------------
+ * David Tougaw and Matthew Bowen
+ * 11/21/2019
  * --------------------------------------
  * 32 bit integer inputs A and B
  * --------------------------------------
@@ -23,9 +23,9 @@
  * ###############################################################################
  */
 
-module GCD_HW_avalon_interface (clock, resetn, writedata, address, readdata, write, read, chipselect);
+module String_HW_avalon_interface (clock, reset, writedata, address, readdata, write, read, chipselect);
    // signals for connecting to the Avalon fabric
-   input logic clock, resetn, read, write, chipselect;
+   input logic clock, reset, read, write, chipselect;
    input logic [2:0] address;
    input logic [31:0] writedata;
    output logic [31:0] readdata;
@@ -69,5 +69,29 @@ module GCD_HW_avalon_interface (clock, resetn, writedata, address, readdata, wri
 			else if(control_register & (address==3))
 				control_register<=0;
 		end
+		
+		
+		
+/* 
+	// Read data into FIFO and wait for go signal
+				S1: 	begin 
+							// Wait for go or FIFO_read flag
+							if (go) nextstate <= S2; 
+							else if (FIFO_write) begin
+								// Write to FIFO_1
+								if (FIFO_select) begin	
+									FIFO_1_in[memory_index_1] <= data_in; // Read 32 bits of data from FIFO
+									memory_index_1 <= memory_index_1 + 1;		// increment memory index for FIFO 1
+									nextstate <= S1;
+									end
+								// Write to FIFO_0
+								else 		
+									FIFO_0_in[memory_index_0] <= data_in; // Read 32 bits of data from FIFO
+									memory_index_0 <= memory_index_0 + 1;		// increment memory index for FIFO 0
+									nextstate <= S1;
+									end
+							else 
+								nextstate <= S1;				 // Do nothing, wait for go or read flag
+*/
 	
 endmodule
